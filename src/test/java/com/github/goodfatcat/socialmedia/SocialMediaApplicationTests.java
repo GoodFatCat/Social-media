a691cc3c81492e83db4732c39fe1b47b48005cd9
GoodFatCat <osertupokrushki92@gmail.com>
2023-05-27 08:56:44 +0700

    Init commit

---
+++ src/test/java/com/github/goodfatcat/socialmedia/SocialMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.goodfatcat.socialmedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
